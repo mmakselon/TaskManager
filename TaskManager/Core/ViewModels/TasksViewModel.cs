b3dad931b4c67b347964593aed004a393aefeb76
mmakselon <magdalena.pachacz@gmail.com>
2024-11-18 11:26:53 +0100

    add new ViewModels

---
+++ TaskManager/Core/ViewModels/TasksViewModel.cs
@@ -0,0 +1,13 @@
+﻿using TaskManager.Core.Models.Domains;
+using Task=TaskManager.Core.Models.Domains.Task;
+
+namespace TaskManager.Core.ViewModels
+{
+    public class TasksViewModel
+    {
+        public IEnumerable<Task> Tasks { get; set; }
+
+        public IEnumerable<Category> Categories { get; set; }
+        public FilterTasks FilterTasks { get; set; }
+    }
+}
