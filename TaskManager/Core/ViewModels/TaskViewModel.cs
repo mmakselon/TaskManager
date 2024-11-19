8b3856498c20e42e829c3e054610d739ad3fbdf3
mmakselon <magdalena.pachacz@gmail.com>
2024-11-19 15:58:04 +0100

    update Task view

---
+++ TaskManager/Core/ViewModels/TaskViewModel.cs
@@ -0,0 +1,12 @@
+﻿using TaskManager.Core.Models.Domains;
+using Task = TaskManager.Core.Models.Domains.Task;
+
+namespace TaskManager.Core.ViewModels
+{
+    public class TaskViewModel
+    {
+        public string Heading { get; set; }
+        public Task Task { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
