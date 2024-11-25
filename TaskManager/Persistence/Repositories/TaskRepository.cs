92407a0db5f5b68aee7b0550f5270b4c7da70d0b
mmakselon <magdalena.pachacz@gmail.com>
2024-11-25 23:00:14 +0100

    add Get and GetCategories method

--- TaskManager/Persistence/Repositories/TaskRepository.cs
+++ TaskManager/Persistence/Repositories/TaskRepository.cs
@@ -1,4 +1,5 @@
-﻿using TaskManager.Core.Models.Domains;
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManager.Core.Models.Domains;
 using Task = TaskManager.Core.Models.Domains.Task;
 
 namespace TaskManager.Persistence.Repositories
@@ -12,17 +13,28 @@ namespace TaskManager.Persistence.Repositories
                 _context = context; 
         }
 
-        public IEnumerable<Task> Get(string userId, bool isExecuted = false, int categoryId = 0, string titl = null)
+        public IEnumerable<Task> Get(string userId, bool isExecuted = false, int categoryId = 0, string title = null)
         {
-            throw new NotImplementedException();
-        }
+            var tasks = _context.Tasks
+                .Include(x=> x.Category)
+                .Where(x=>x.UserId==userId &&
+                x.IsExecuted == isExecuted);
 
-        public Task Get(int id, string userId)
-        {
-            throw new NotImplementedException();
+            if (categoryId!= 0)
+                tasks = tasks.Where(x => x.CategoryId == categoryId);
+
+            if(!string.IsNullOrWhiteSpace(title))
+                tasks = tasks.Where(x => x.Title.Contains(title));
+
+            return tasks.OrderBy(x=>x.Term).ToList();
         }
 
         public IEnumerable<Category> GetCategories()
+        {
+            return _context.Categories.OrderBy(x=>x.Name).ToList();
+        }
+
+        public Task Get(int id, string userId)
         {
             throw new NotImplementedException();
         }
