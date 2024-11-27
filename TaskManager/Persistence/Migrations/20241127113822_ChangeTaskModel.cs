ce1858a6eab3ab6f81f43c5c5716c25d33ac7e58
mmakselon <magdalena.pachacz@gmail.com>
2024-11-27 13:01:44 +0100

    change Task model

---
+++ TaskManager/Persistence/Migrations/20241127113822_ChangeTaskModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManager.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeTaskModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsExecute",
+                table: "Tasks",
+                newName: "IsExecuted");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsExecuted",
+                table: "Tasks",
+                newName: "IsExecute");
+        }
+    }
+}
