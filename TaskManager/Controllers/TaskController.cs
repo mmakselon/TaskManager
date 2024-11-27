ad9e3bf1582d2f0d902cf397ba3c3eca23eed880
mmakselon <magdalena.pachacz@gmail.com>
2024-11-27 22:08:27 +0100

    change views

--- TaskManager/Controllers/TaskController.cs
+++ TaskManager/Controllers/TaskController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using TaskManager.Core.ViewModels;
--- TaskManager/Core/Models/Domains/ApplicationUser.cs
+++ TaskManager/Core/Models/Domains/ApplicationUser.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace TaskManager.Core.Models.Domains
 {
