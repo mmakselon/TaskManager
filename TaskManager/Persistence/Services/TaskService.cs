7aa6a4848abc59fbedcf066ecc3143c053aabff9
mmakselon <magdalena.pachacz@gmail.com>
2024-12-01 10:52:12 +0100

    add class TaskService

---
+++ TaskManager/Persistence/Services/TaskService.cs
@@ -0,0 +1,59 @@
+﻿using TaskManager.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TaskManager.Core.Models.Domains;
+using Task = TaskManager.Core.Models.Domains.Task;
+
+namespace MyTasks.Persistence.Services
+{
+    public class TaskService
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public TaskService(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IEnumerable<Task> Get(string userId,
+            bool isExecuted = false, int categoryId = 0, string title = null)
+        {
+            return _unitOfWork.Task.Get(userId, isExecuted, categoryId, title);
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return _unitOfWork.Task.GetCategories();
+        }
+
+        public Task Get(int id, string userId)
+        {
+            return _unitOfWork.Task.Get(id, userId);
+        }
+
+        public void Add(Task task)
+        {
+            _unitOfWork.Task.Add(task);
+            _unitOfWork.Complete();
+        }
+
+        public void Update(Task task)
+        {
+            _unitOfWork.Task.Update(task);
+            _unitOfWork.Complete();
+        }
+
+        public void Delete(int id, string userId)
+        {
+            _unitOfWork.Task.Delete(id, userId);
+            _unitOfWork.Complete();
+        }
+
+        public void Finish(int id, string userId)
+        {
+            _unitOfWork.Task.Finish(id, userId);
+            _unitOfWork.Complete();
+        }
+    }
+}
