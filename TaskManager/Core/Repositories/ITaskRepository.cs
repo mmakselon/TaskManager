f4d530f4e26d92a656265b40072e023ecc08591c
mmakselon <magdalena.pachacz@gmail.com>
2024-12-03 22:23:26 +0100

    add ITaskRepository

---
+++ TaskManager/Core/Repositories/ITaskRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using TaskManager.Core.Models.Domains;
+using Task = TaskManager.Core.Models.Domains.Task;
+
+namespace MyTasks.Core.Repositories
+{
+    public interface ITaskRepository
+    {
+        IEnumerable<Task> Get(string userId,
+            bool isExecuted = false, int categoryId = 0, string title = null);
+        IEnumerable<Category> GetCategories();
+        Task Get(int id, string userId);
+        void Add(Task task);
+        void Update(Task task);
+        void Delete(int id, string userId);
+        void Finish(int id, string userId);
+    }
+}
