e2f34006943fe93271d716d9643215aba4975e4d
mmakselon <magdalena.pachacz@gmail.com>
2024-12-04 22:51:43 +0100

    add ITaskService

---
+++ TaskManager/Core/Service/ITaskService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TaskManager.Core.Models.Domains;
+using Task = TaskManager.Core.Models.Domains.Task;
+
+namespace MyTasks.Core.Service
+{
+    public interface ITaskService
+    {
+        IEnumerable<Task> Get(string userId,
+            bool isExecuted = false, int categoryId = 0, string title = null);
+        IEnumerable<Category> GetCategories();
+        Task Get(int id, string userId);
+        void Add(Task task);
+        void Update(Task task);
+        void Delete(int id, string userId);
+        void Finish(int id, string userId);
+    }
+}
