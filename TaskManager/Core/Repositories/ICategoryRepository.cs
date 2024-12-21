6fe2490620aaaa3efa4b63bfaabc120cbab93c40
mmakselon <magdalena.pachacz@gmail.com>
2024-12-21 22:00:47 +0100

    add method to interface

--- TaskManager/Core/Repositories/ICategoryRepository.cs
+++ TaskManager/Core/Repositories/ICategoryRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace TaskManager.Core.Repositories
+﻿using TaskManager.Core.Models.Domains;
+
+namespace TaskManager.Core.Repositories
 {
     public interface ICategoryRepository
     {
+        IEnumerable<Category> Get(string userId);
+        Category Get(int id, string userId);
+        void Add(Category category);
+        void Update(Category category);
+        void Delete(int id, string userId);
     }
 }
