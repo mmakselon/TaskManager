09878b6367cdba9532f428a0bb66880d6b0c9d3e
mmakselon <magdalena.pachacz@gmail.com>
2024-12-22 12:36:05 +0100

    add context

--- TaskManager/Persistence/Repositories/CategoryRepository.cs
+++ TaskManager/Persistence/Repositories/CategoryRepository.cs
@@ -1,10 +1,18 @@
-﻿using TaskManager.Core.Models.Domains;
+﻿using TaskManager.Core;
+using TaskManager.Core.Models.Domains;
 using TaskManager.Core.Repositories;
 
 namespace TaskManager.Persistence.Repositories
 {
     public class CategoryRepository : ICategoryRepository
     {
+        private IApplicationDbContext _context;
+        public CategoryRepository(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
         public void Add(Category category)
         {
             throw new NotImplementedException();
