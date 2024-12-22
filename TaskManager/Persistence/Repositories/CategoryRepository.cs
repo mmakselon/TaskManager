2c7d7cd9496680f233340b33221aa9384cc8e636
mmakselon <magdalena.pachacz@gmail.com>
2024-12-22 12:46:14 +0100

    update CategoryRepository

--- TaskManager/Persistence/Repositories/CategoryRepository.cs
+++ TaskManager/Persistence/Repositories/CategoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using TaskManager.Core;
+﻿using System.Threading.Tasks;
+using TaskManager.Core;
 using TaskManager.Core.Models.Domains;
 using TaskManager.Core.Repositories;
 
@@ -15,27 +16,33 @@ namespace TaskManager.Persistence.Repositories
 
         public void Add(Category category)
         {
-            throw new NotImplementedException();
+            _context.Categories.Add(category);
         }
 
         public void Delete(int id, string userId)
         {
-            throw new NotImplementedException();
+            var categoryToDelete = _context.Categories.Single(x=>x.Id == id && x.UserId == userId);
+            _context.Categories.Remove(categoryToDelete);
         }
 
         public IEnumerable<Category> Get(string userId)
         {
-            throw new NotImplementedException();
+            return _context.Categories
+                .Where(x=>x.UserId == userId)
+                .OrderBy(x=>x.Name);
         }
 
         public Category Get(int id, string userId)
         {
-            throw new NotImplementedException();
+            return _context.Categories
+                .Single(x => x.UserId == userId && x.Id == id);
         }
 
         public void Update(Category category)
         {
-            throw new NotImplementedException();
+            var categoryToUpdate = _context.Categories.Single(x => x.Id == category.Id);
+
+            categoryToUpdate.Name = category.Name;
         }
     }
 }
