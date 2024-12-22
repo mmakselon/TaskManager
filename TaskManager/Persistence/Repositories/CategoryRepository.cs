ed82ff853f6287f44b782d378f8e702a1ab0cc78
mmakselon <magdalena.pachacz@gmail.com>
2024-12-22 12:31:25 +0100

    implement interface ICategoryRepository in CategoryRepository

--- TaskManager/Persistence/Repositories/CategoryRepository.cs
+++ TaskManager/Persistence/Repositories/CategoryRepository.cs
@@ -1,6 +1,33 @@
-﻿namespace TaskManager.Persistence.Repositories
+﻿using TaskManager.Core.Models.Domains;
+using TaskManager.Core.Repositories;
+
+namespace TaskManager.Persistence.Repositories
 {
-    public class CategoryRepository
+    public class CategoryRepository : ICategoryRepository
     {
+        public void Add(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Category> Get(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category Get(int id, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category category)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
