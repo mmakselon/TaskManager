613dcc566cc9798346fb43401b95596560e9e24c
mmakselon <magdalena.pachacz@gmail.com>
2024-12-23 22:51:09 +0100

    add CategoryService

---
+++ TaskManager/Persistence/Services/CategoryService.cs
@@ -0,0 +1,33 @@
+﻿using TaskManager.Core.Models.Domains;
+using TaskManager.Core.Service;
+
+namespace TaskManager.Persistence.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        public void Add(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Category> Get(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category Get(int id, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
